b0b83c170cfee3987b98f645e969ff74118ec5e6
alumno <alumno>
2019-02-05 19:08:19 -0300

    tercer guardado 2da clase

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,11 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+   var primero = document.getElementById("numeroUno").value; 
+   var segundo = document.getElementById("numeroDos").value;
+    parseInt(primero);
+    parseInt(segundo);
+    var resultado = primero + segundo;
+   alert(resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,41 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primero = document.getElementById("numeroUno").value; 
+    var segundo = document.getElementById("numeroDos").value;
+    parseInt(primero);
+    parseInt(segundo);
+    var resultado = primero + segundo;
+   alert(resultado);
 }
 
 function restar()
 {
-	
+	var primero = document.getElementById("numeroUno").value; 
+    var segundo = document.getElementById("numeroDos").value;
+    parseInt(primero);
+    parseInt(segundo);
+    var resultado = primero - segundo;
+   alert(resultado);
 }
 
 function multiplicar()
 { 
-	
+	var primero = document.getElementById("numeroUno").value; 
+    var segundo = document.getElementById("numeroDos").value;
+    parseInt(primero);
+    parseInt(segundo);
+    var resultado = primero * segundo;
+   alert(resultado);
 }
 
 function dividir()
 {
-	
+	var primero = document.getElementById("numeroUno").value; 
+    var segundo = document.getElementById("numeroDos").value;
+    parseInt(primero);
+    parseInt(segundo);
+    var resultado = primero / segundo;
+   alert(resultado);
 }
 
