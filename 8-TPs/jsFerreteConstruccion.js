24e61b7d66ec9ded431777f7e03e33d51902134d
alumno <alumno>
2019-02-11 22:05:54 -0300

    guardado 3

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -3,16 +3,26 @@ A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
-
+var largo, ancho, radio;
 function Rectangulo () 
 {
-
+    largo = parseInt(document.getElementById("Largo").value);
+    ancho = parseInt(document.getElementById("Ancho").value);
+    var perimetro = (largo + ancho)*2;
+    alert(perimetro * 3);
 }
 function Circulo () 
 {
-	
+	radio = parseInt(document.getElementById("Radio").value);
+    var mostrar = Math.floor(radio * Math.PI * 2);
+    alert(mostrar * 3);
 }
 function Materiales () 
 {
-	
+	largo = parseInt(document.getElementById("Largo").value);
+    ancho = parseInt(document.getElementById("Ancho").value);
+    var per = (largo * ancho);
+    var volCem = (per * 2);
+    var volCal = (per * 3);
+    alert("Son " + volCem + " bolsas de cemento y " + volCal + " bolsas de Cal.")
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,28 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+var preUno, preDos, preTres;
 function Sumar () 
 {
-	
+    preUno = parseInt(document.getElementById("PrecioUno").value);
+    preDos = parseInt(document.getElementById("PrecioDos").value);
+    preTres = parseInt(document.getElementById("PrecioTres").value);
+    var suma = preUno + preDos + preTres;
+    alert("La suma es " + suma);
 }
 function Promedio () 
 {
-	
+	preUno = parseInt(document.getElementById("PrecioUno").value);
+    preDos = parseInt(document.getElementById("PrecioDos").value);
+    preTres = parseInt(document.getElementById("PrecioTres").value);
+    alert((preUno + preDos + preTres)/3);
 }
 function PrecioFinal () 
 {
-	
+	preUno = parseInt(document.getElementById("PrecioUno").value);
+    preDos = parseInt(document.getElementById("PrecioDos").value);
+    preTres = parseInt(document.getElementById("PrecioTres").value);
+    var final = (preUno + preDos + preTres);
+    var iva = ((final * 21) / 100);
+    alert(final + iva);
 }
