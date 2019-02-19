439463b997d6617bface52cb76b7d1b27348af21
alumno <alumno>
2019-02-18 22:13:01 -0300

    ultimo guardado

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,8 @@ function mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
-
+	while(numero > 9 || numero < 0){
+		numero = prompt("ingrese un nuevo numero");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,8 +3,10 @@ function mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
-
+    while(sexo == f || sexo == m){
+        sexo = prompt("Sexo no valido, reintentar");
+    }
 
 document.getElementById('Sexo').value=sexo;
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN  ACA ME QUEDEEEEEEEEEEEEE TENGO Q SEGUIRLO Y HACERLO BIEN
