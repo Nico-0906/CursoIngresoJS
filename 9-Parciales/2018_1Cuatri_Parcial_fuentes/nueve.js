154614dd6648cb1e3a62de1f25bc9f814b211361
nico-0906 <nico1844@gmail.com>
2019-02-26 22:21:43 -0300

    new save

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+  var marca;
+  var peso;
+  var temp;
 
+while(confirm("Desea ingresar otro producto?")){
+marca = prompt("ingrese la marca");
+
+peso = prompt("ingrese peso");
+if(peso>100 || peso<1){
+  peso = prompt("reingrese peso entre 1 y 100");
+}
+temp = prompt("ingrese temperatura");
+if(temp>30 || temp<-30){
+  temp = prompt("Reingrese temperatura entre 30 y -30");
+}
+}
+
+
+
+
+
+
+
+document.write();
 }
