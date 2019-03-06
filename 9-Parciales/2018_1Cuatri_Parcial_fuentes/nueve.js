66a2f3417935713efb3fcde663b8b47b07cdad0a
nico-0906 <nico1844@gmail.com>
2019-03-06 09:19:22 -0300

    completado de ejercicios parcial 7 8 9

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -3,24 +3,60 @@ function mostrar()
   var marca;
   var peso;
   var temp;
-
+  var canttemppar = 0;
+  var primerp = true;
+  var pesado;
+  var contador = 0;
+  var prodpesado;
+  var contcer = 0;
+  var aux = 0;
+  var minimo = 0;
+  var maximo = 0;
+  var producto;
+while(confirm("Desea ingresar producto?")){
 marca = prompt("ingrese la marca");
-
 peso = prompt("ingrese peso");
+peso = parseInt(peso);
 if(peso>100 || peso<1){
   peso = prompt("reingrese peso entre 1 y 100");
+  peso = parseInt(peso);
+}else{
+  if(primerp){
+    primerp=false;
+    pesado = peso;
+    minimo = peso;
+    prodpesado = marca;
+  }else{
+    if(peso > pesado){
+      prodpesado = marca;
+      pesado = peso;
+    }else{
+      if(peso < minimo){
+        minimo = peso;
+      }
+    }
+  }
 }
 temp = prompt("ingrese temperatura");
 if(temp>30 || temp<-30){
   temp = prompt("Reingrese temperatura entre 30 y -30");
+}else{
+  if(temp < 0){
+    contcer++;
+  }
 }
-
-
-
-
-
-
-
-
-document.write();
+temp = parseInt(temp);
+if(temp % 2 == 0){
+  canttemppar++;
+}
+aux = aux + peso;
+contador++;
+}
+producto = aux / contador;
+producto = Math.round(producto);
+document.writeln("A - La cantidad de temperaturas pares son " + canttemppar);
+document.writeln("     B - La marca del producto mas pesado es " + prodpesado);
+document.writeln("     C - La cantidad de productos que se conservan a menos de 0 grados son " + contcer);
+document.writeln("     D - El promedio del peso de todos los productos es " + producto);
+document.writeln("     E - El peso maximo es " + pesado + " y el peso minimo es " + minimo);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,41 @@
 function mostrar()
 {
-
+var nota;
+var sexo;
+var contador = 0;
+var sumanotas;
+var promedio = 0;
+var min;
+var primera = true;
+var aux;
+var contadorhombre = 0;
+while(contador < 5){
+    contador++;
+    nota = prompt("ingrese nota");
+    if(nota < 0 || nota > 10){
+        nota = prompt("Reingrese nota entre 0 y 1");
+    }else{
+        if(sexo == "M" && nota >= 6){
+            contadorhombre++;
+        }
+        if(primera){
+            primera = false;
+            min = nota;
+        }else{
+            if(nota < min){
+                min = nota;
+                aux = sexo;
+            }
+        }
+        sumanotas = sumanotas + nota;
+    }
+    sexo = prompt("ingrese sexo F/M ");
+    if(sexo != "F" && sexo != "M"){
+        sexo = prompt("Reingrese sexo valido");
+    }
+}
+promedio = sumanotas / 5;
+alert("El promedio de las notas totales es " + promedio);
+alert("La nota mas baja es " + min + " y el sexo de esa persona es " + aux);
+alert("La cantidad de varones que su nota fue igual o mayor a 6 es de " + contadorhombre);
 }
